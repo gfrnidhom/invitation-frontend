8b824a9fed48ac01ee22bc61f63ac9f362755d3b
gfrnidhom <gfr.nidhom@gmail.com>
2026-04-05 11:24:55 +0700

    chore: update default API_URL to production endpoint

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 'use client';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://app.digitvitation.my.id/api';
 
 function getToken() {
   if (typeof window !== 'undefined') {
